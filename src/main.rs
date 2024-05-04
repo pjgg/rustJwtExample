87f272350ab8d97e82db78b27f9b2212e56f37e7
pablo gonzalez granados <pablo.granados@hivemq.com>
2024-05-04 19:26:18 +0200

    MultiThread impl

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,88 @@
 use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
-use std::env;
 use std::fs;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::{Duration, SystemTime};
+
+const TOTAL_ITERATIONS: usize = 1_000_000;
+const AMOUNT_THREAD: usize = 32;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
     sub: String,
-    iat: u128,
-    exp: u128,
+    iat: u64,
+    exp: u64,
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
     let file_contents =
         fs::read_to_string("/home/pagonzal/Documents/workspace/rustExample/emails.json")
             .unwrap();
     let emails: Vec<String> = serde_json::from_str(&file_contents).unwrap();
-
-    let mut i = 1;
-    let mut idx = 0;
-    let jwt_secret = env::var("JWT_SECRET").expect("$JWT_SECRET is not set");
+    let jwt_secret = std::fs::read_to_string("/home/pagonzal/Documents/workspace/jwtSignExample/src/main/resources/private_key.pem").unwrap();
     let jwt_encoding_key = EncodingKey::from_secret(jwt_secret.as_bytes());
-    let jwt_decoding_key = DecodingKey::from_secret(jwt_secret.as_bytes());
-    let num_iterations = args[1].parse::<i32>().unwrap();
-    let mut start_ts = std::time::UNIX_EPOCH.elapsed().unwrap().as_millis();
-    let validation = Validation::new(Algorithm::HS256);
+    let total_op = Arc::new(AtomicUsize::new(0));
 
-    loop {
-        let email = &emails[idx];
-        idx += 1;
-        let curr_ts = std::time::UNIX_EPOCH.elapsed().unwrap().as_millis();
-        let my_claims = Claims {
-            sub: email.to_string(),
-            iat: curr_ts,
-            exp: curr_ts + 2 * 60 * 60 * 1000,
-        };
-        
-        let token = match encode(&Header::default(), &my_claims, &jwt_encoding_key) {
-            Ok(t) => t,
-            Err(_) => panic!(),
-        };
-        println!("Token: {}", token);
-        let token_data = match decode::<Claims>(&token, &jwt_decoding_key, &validation) {
-            Ok(c) => c,
-            Err(err) => panic!("{}", err.to_string()),
-        };
-        if token_data.claims.sub != email.to_string() {
-            panic!("email didn't match");
-        }
-        if idx >= emails.len() {
-            idx = 0;
-        }
-        i += 1;
-        if i > num_iterations {
-            break;
-        }
+    let mut handles = vec![];
+
+    for _ in 0..AMOUNT_THREAD {
+        let emails_clone = emails.clone();
+        let jwt_encoding_key_clone = jwt_encoding_key.clone();
+        let total_op_clone = Arc::clone(&total_op);
+
+        let handle = thread::spawn(move || {
+            let jwt_secret_thread = std::fs::read_to_string("/home/pagonzal/Documents/workspace/jwtSignExample/src/main/resources/private_key.pem").unwrap();
+            let jwt_decoding_key_thread = DecodingKey::from_secret(jwt_secret_thread.as_bytes());
+
+            for _ in 0..TOTAL_ITERATIONS {
+                let idx = generate_random_position(99_999);
+                let email = emails_clone[idx].clone();
+                let curr_ts = SystemTime::now()
+                    .duration_since(SystemTime::UNIX_EPOCH)
+                    .unwrap()
+                    .as_millis() as u64;
+                let my_claims = Claims {
+                    sub: email.clone(),
+                    iat: curr_ts,
+                    exp: curr_ts + 2 * 60 * 60 * 1000,
+                };
+                let token = match encode(&Header::default(), &my_claims, &jwt_encoding_key_clone)
+                {
+                    Ok(t) => t,
+                    Err(_) => panic!(),
+                };
+                println!("Token: {}", token);
+                let token_data = match decode::<Claims>(&token, &jwt_decoding_key_thread, &Validation::new(Algorithm::HS256)) {
+                    Ok(c) => c,
+                    Err(err) => panic!("{}", err.to_string()),
+                };
+                if token_data.claims.sub != email {
+                    panic!("email didn't match");
+                }
+                let current_total_op = total_op_clone.fetch_add(1, Ordering::Relaxed);
+                println!("{}", current_total_op);
+                if current_total_op >= TOTAL_ITERATIONS {
+                    break;
+                }
+            }
+        });
+        handles.push(handle);
+    }
+    
+    let mut start_ts = std::time::UNIX_EPOCH.elapsed().unwrap().as_millis();
+    for handle in handles {
+        handle.join().unwrap();
     }
 
     let end_ts = std::time::UNIX_EPOCH.elapsed().unwrap().as_millis();
     let diff = end_ts - start_ts;
     println!("{}", diff);
+}
+
+fn generate_random_position(max: usize) -> usize {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0..=max)
 }
